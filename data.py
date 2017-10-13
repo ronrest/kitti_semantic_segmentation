55914c5044897ecae5000ada8dc374d5178096ac
ronrest <ronny.coding@gmail.com>
2017-10-13 22:30:46 +1100

    FEAT: Add maybe_make_dir and get_pardir

--- data.py
+++ data.py
@@ -8,6 +8,26 @@ import pickle
 
 
 # ==============================================================================
+#                                                                 MAYBE_MAKE_DIR
+# ==============================================================================
+def maybe_make_dir(path):
+    """ Checks if a directory path exists on the system, if it does not, then
+        it creates that directory (and any parent directories needed to
+        create that directory)
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+# ==============================================================================
+#                                                                     GET_PARDIR
+# ==============================================================================
+def get_pardir(file):
+    """ Given a file path, it returns the parent directory of that file. """
+    return os.path.dirname(file)
+
+
+# ==============================================================================
 #                                                              MAYBE_MAKE_PARDIR
 # ==============================================================================
 def maybe_make_pardir(file):
